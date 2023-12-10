4d3d010c4e4896457a58f872c252ac52f9e9af89
Rufai-Ahmed <abbeyrufai234@gmail.com>
2023-12-10 09:45:03 +0100

    created all essential files and folders

---
+++ FE/src/Router/Router.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Layout } from "../components/Layout";
+import { Register } from "../Pages/Auth/Register";
+import { Login } from "../Pages/Auth/Login";
+import { Verification } from "../Pages/Auth/Verification";
+
+export const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/verify",
+    element: <Verification />,
+  },
+]);
