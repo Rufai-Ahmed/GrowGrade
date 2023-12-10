c8559fdba49882049498d84714d76ec4379f603b
Rufai-Ahmed <abbeyrufai234@gmail.com>
2023-12-10 11:38:32 +0100

    App.tsx router provided

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
+import { RouterProvider } from "react-router-dom";
+import { Router } from "./Router/Router";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={Router} />
+    </>
+  );
 }
 
 export default App;
