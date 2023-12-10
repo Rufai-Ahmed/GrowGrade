9ca64895562519bfc21cb8dd9de2da9be3307af4
Dev-Topper~ <rehoboth@gmail.com>
2023-12-10 20:29:10 +0100

    Move

---
+++ BE/controller/schoolController.ts
@@ -0,0 +1,16 @@
+import { Response, Request } from "express";
+import { HTTP } from "../utils/enums";
+
+export const createSchool = async (req: Request, res: Response) => {
+	try {
+		const { email, password } = req.body;
+		return res.status(HTTP.CREATED).json({
+			message: "This school has successfully been created.",
+		});
+	} catch (error) {
+		return res.status(HTTP.BAD).json({
+			message:
+				"Sorry!! User not created. There was an error with this request.",
+		});
+	}
+};
