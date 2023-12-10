8434a1662fcee6f45a60f36ee23abd6dd3e48b6b
Dev-Topper~ <rehoboth@gmail.com>
2023-12-10 16:53:29 +0100

    LOG In

--- FE/src/Router/Router.tsx
+++ FE/src/Router/Router.tsx
@@ -5,20 +5,20 @@ import { Login } from "../Pages/Auth/Login";
 import { Verification } from "../Pages/Auth/Verification";
 
 export const Router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/verify",
-    element: <Verification />,
-  },
+	{
+		path: "/",
+		element: <Layout />,
+	},
+	{
+		path: "/register",
+		element: <Register />,
+	},
+	{
+		path: "/login",
+		element: <Login />,
+	},
+	{
+		path: "/verify",
+		element: <Verification />,
+	},
 ]);
