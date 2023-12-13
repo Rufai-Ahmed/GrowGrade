716c1fae7d07652240b72581704a55b5183030da
Ayomide <ogunyemiayomide700@gmail.com>
2023-12-13 03:15:41 +0100

    minor changes

--- FE/src/Pages/Dashboards/Homes/Admin.tsx
+++ FE/src/Pages/Dashboards/Homes/Admin.tsx
@@ -1,9 +1,11 @@
 import { CiBellOn } from "react-icons/ci";
 import { CiSearch } from "react-icons/ci";
+import { BsThreeDots } from "react-icons/bs";
+import pic from "../../../Assets/SCR-20231213-dldu.png";
 
 export const Admin = () => {
   return (
-    <div className="p-[20px]  w-full h-screen bg-slate-200 ">
+    <div className="p-[20px]  w-[calc(100%-250px)] h-screen bg-slate-200 ">
       <div className="w-full h-[80px] bg-slate-600 rounded-md flex justify-between items-center ">
         <div className="pl-[18px] text-[23px] text-white font-bold ">
           Dashboard
@@ -15,7 +17,7 @@ export const Admin = () => {
           </div>
           <input
             type="text"
-            className="w-full h-full rounded-md "
+            className="w-full h-full rounded-md pl-4 outline-none"
             placeholder="Search for students/teachers/documents"
           />
         </div>
@@ -58,9 +60,102 @@ export const Admin = () => {
         </div>
       </div>
 
-      <div className="w-full bg-zinc-500 h-full flex">
-        <div className="w-[70%] h-full bg-red-600"></div>
-        <div className="w-[30%] h-full bg-green-50"></div>
+      <div className="w-full h-[79%]  grid grid-cols-3 gap-6 ">
+        <div className="  grid col-span-2 gap-6">
+          <div className="bg-white col-span-2 rounded-md p-4">
+            <h2 className="text-[22px] font-bold text-slate-600">
+              Total Earnings
+            </h2>
+            {/* <img src={pic} alt="" className="w-full h-[80%] object-contain" /> */}
+          </div>
+          <div className="bg-white rounded-md p-4 ">
+            {" "}
+            <div className="flex justify-between">
+              <h2 className="text-[22px] font-bold text-slate-600">
+                Total Performers
+              </h2>
+              <div className="text-[25px] text-gray-300">
+                <BsThreeDots />
+              </div>
+            </div>
+            {/* <div>
+              <div className="flex gap-[55px] text-[16px] mx-5 -5 -3">
+                <div className="">Week</div>
+                <div>Month</div>
+                <div>Year</div>
+              </div>
+              <hr />
+            </div> */}
+            {/* <div className="grid grid-cols-6 mt-4">
+              <div className="bg-red-50">
+                <div className="-7">Photo</div>
+                <div className="w-[40px] h-[40px] rounded-full border -3"></div>
+              </div>
+              <div className="bg-red-100 col-span-2">
+                <div className="-7">Name</div>
+                <div className="-6 text-[18px] font-semibold ">
+                  samuel ayomide
+                </div>
+                <div className="-6 text-[18px] font-semibold ">
+                  samuel ayomide
+                </div>
+                <div className="-6 text-[18px] font-semibold ">
+                  samuel ayomide
+                </div>
+                <div className="-6 text-[18px] font-semibold ">
+                  samuel ayomide
+                </div>
+              </div>
+              <div className="bg-red-200">
+                <div className="-7">ID Nuer</div>
+                <div className="-6 text-[18px] font-semibold ">ID:1243</div>
+                <div className="-6 text-[18px] font-semibold ">ID:1243</div>
+                <div className="-6 text-[18px] font-semibold ">ID:1243</div>
+                <div className="-6 text-[18px] font-semibold ">ID:1243</div>
+              </div>
+              <div className="bg-red-300">
+                <div className="-7">Standard</div>
+                <div className="-6 text-[18px] font-semibold ">6th</div>
+                <div className="-6 text-[18px] font-semibold ">6th</div>
+                <div className="-6 text-[18px] font-semibold ">6th</div>
+                <div className="-6 text-[18px] font-semibold ">6th</div>
+              </div>
+              <div className="bg-red-400">fkfrkrr</div>
+            </div> */}
+          </div>
+          <div className="bg-white rounded-md p-4">
+            {" "}
+            <div className="flex justify-between">
+              <h2 className="text-[22px] font-bold text-slate-600">
+                Attendance
+              </h2>
+              <div className="text-[25px] text-gray-300">
+                <BsThreeDots />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="  rounded-md grid grid-rows-3 gap-5 ">
+          <div className="bg-white row-span-2 rounded-md p-4">
+            <div className="flex justify-between">
+              <h2 className="text-[22px] font-bold text-slate-600">
+                Events calendar
+              </h2>
+              <div className="text-[25px] text-gray-300">
+                <BsThreeDots />
+              </div>
+            </div>
+          </div>
+          <div className="bg-slate-600 rounded-md p-4">
+            <div className="text-[25px] text-white w-[60%] font-semibold ">
+              Join the community and find out more ...
+            </div>
+
+            <button className="bg-white py-4 px-[30px] mt-[75px] rounded-md">
+              Explore Now
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
